e1ae29ee341348648eeb0c4ed54ad35ddbaaa0fb
Karlo Dela Rosa <karlodelarosa@Karlos-MacBook-Air.local>
2023-08-17 23:13:24 +0800

    Fixed index error

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -24,7 +24,7 @@ const redirect = (url: any) => {
       <Brand brand-name="COMPANY" />
 
       <ul class="space-y-2 font-medium">
-        <li v-for="items in SIDEBAR_ITEMS" @click="redirect(items.redirection)" :key="items">
+        <li v-for="(items, index) in SIDEBAR_ITEMS" @click="redirect(items.redirection)" :key="index">
           <a
             href="#"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-teal-400 dark:hover:bg-gray-800 group"
