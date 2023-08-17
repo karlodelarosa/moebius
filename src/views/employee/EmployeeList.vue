4433b508e8ff44c46421d2bfb4d04868bbdf29b8
Karlo Dela Rosa <karlodelarosa@Karlos-MacBook-Air.local>
2023-08-17 22:35:15 +0800

    Added filter toggle

--- src/views/employee/EmployeeList.vue
+++ src/views/employee/EmployeeList.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import InputText from '@/components/inputs/InputText.vue'
 
@@ -10,6 +11,11 @@ const addEmployee = () => {
 const viewDetails = () => {
   router.push('/employee/details')
 }
+
+const isOpenFilter = ref(false)
+const toggleFilter = () => {
+  isOpenFilter.value =!isOpenFilter.value
+}
 </script>
 
 <template>
@@ -19,15 +25,23 @@ const viewDetails = () => {
         <InputText placeholder="Search ..." />
       </div>
 
-      <div class="flex flex-row gap-3">
+      
+
+      <div class="flex flex-row gap-3 items-center">
+        <!-- <label class="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" value="" class="sr-only peer">
+          <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Smart Search</span>
+        </label> -->
         <div class="relative">
           <button
+            @click="toggleFilter()"
             id="dropdownHelperButton"
             data-dropdown-toggle="dropdownHelper"
             class="text-gray-800 bg-white border border-gray-200 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             type="button"
           >
-            Filter
+            Quick Filter
             <svg
               class="w-2.5 h-2.5 ml-2.5"
               aria-hidden="true"
@@ -48,7 +62,8 @@ const viewDetails = () => {
           <!-- Dropdown menu -->
           <div
             id="dropdownHelper"
-            class="z-10 hidden shadow-lg absolute right-0 bg-white divide-y divide-gray-100 rounded-lg w-60 dark:bg-gray-700 dark:divide-gray-600"
+            class="z-10 shadow-lg absolute right-0 bg-white divide-y divide-gray-100 rounded-lg w-60 dark:bg-gray-700 dark:divide-gray-600"
+            :class="{ 'hidden': !isOpenFilter }"
           >
             <ul
               class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
@@ -70,12 +85,12 @@ const viewDetails = () => {
                       for="helper-checkbox-1"
                       class="font-medium text-gray-900 dark:text-gray-300"
                     >
-                      <div>Enable notifications</div>
+                      <div>New Employee</div>
                       <p
                         id="helper-checkbox-text-1"
                         class="text-xs font-normal text-gray-500 dark:text-gray-300"
                       >
-                        Some helpful instruction goes over here.
+                        Employees that were added less than 6-months ago
                       </p>
                     </label>
                   </div>
@@ -97,39 +112,12 @@ const viewDetails = () => {
                       for="helper-checkbox-2"
                       class="font-medium text-gray-900 dark:text-gray-300"
                     >
-                      <div>Enable 2FA auth</div>
+                      <div>Active Account</div>
                       <p
                         id="helper-checkbox-text-2"
                         class="text-xs font-normal text-gray-500 dark:text-gray-300"
                       >
-                        Some helpful instruction goes over here.
-                      </p>
-                    </label>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                  <div class="flex items-center h-5">
-                    <input
-                      id="helper-checkbox-3"
-                      aria-describedby="helper-checkbox-text-3"
-                      type="checkbox"
-                      value=""
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                    />
-                  </div>
-                  <div class="ml-2 text-sm">
-                    <label
-                      for="helper-checkbox-3"
-                      class="font-medium text-gray-900 dark:text-gray-300"
-                    >
-                      <div>Subscribe newsletter</div>
-                      <p
-                        id="helper-checkbox-text-3"
-                        class="text-xs font-normal text-gray-500 dark:text-gray-300"
-                      >
-                        Some helpful instruction goes over here.
+                        Active employee as of this moment
                       </p>
                     </label>
                   </div>
